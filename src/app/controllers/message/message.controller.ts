9b3ea5b509457eb244e55141e26e81fd3086cc7b
Raphael Ndauwa (Ubuntu) <raphaelndauwa@gmail.com>
2020-02-10 11:30:48 +0300

    add message controller

---
+++ src/app/controllers/message/message.controller.ts
@@ -0,0 +1,21 @@
+import IControllerBase from "../interfaces/IControllerBase.interface";
+import express, {Request, Response} from "express";
+import { KafkaClient as Client } from 'kafka-node';
+
+
+class MessageController implements IControllerBase{
+    public path = '/';
+    public router = express.Router();
+
+    constructor() {
+        this.initRoutes()
+    }
+
+    public initRoutes() {
+        this.router.get('/message', this.index)
+    }
+
+    index = (req: Request, res: Response) => {
+
+    }
+}
