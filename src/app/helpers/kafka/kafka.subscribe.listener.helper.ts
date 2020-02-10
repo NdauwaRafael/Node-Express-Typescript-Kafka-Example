ed9efe8331bad66e972406ea98690e6a6143aac3
Raphael Ndauwa (Ubuntu) <raphaelndauwa@gmail.com>
2020-02-10 16:39:55 +0300

    cleanup

--- src/app/helpers/kafka/kafka.subscribe.listener.helper.ts
+++ src/app/helpers/kafka/kafka.subscribe.listener.helper.ts
@@ -10,7 +10,6 @@ export const kafkaSubscribe: any = () => {
                     const messageObj = new MessageController;
                     await messageObj.save(message.value);
 
-                    console.log(message, message.offset)
                 }
 
             }
